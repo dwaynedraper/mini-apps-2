62fce0884f15ed60024d1b112631be950b6e2d7d
Dwayne Draper <deandraper82@gmail.com>
2021-05-03 20:47:21 -0500

    switching

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,29 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currRound: 1,
+      thisMove: [],
+      nextMove: [],
+      moveAfterNext: [],
+      numPins: '',
+      message: ''
+    }
+  }
+  componentDidMount() {
 
+  }
+  render() {
+    return (
+      <div>
+        <PinSelector />
+        <Scorecard />
+        <MessageBoard />
+      </div>
+    )
+  }
+}
+
+export default App;
