955229aacdfe985e43bdd8398e468dfc5cc4d6c7
Dwayne Draper <deandraper82@gmail.com>
2021-05-04 14:27:27 -0500

    Switching devices

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -5,7 +5,8 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      currRound: 1,
+      currFrame: 1,
+      currTurn: 1,
       thisMove: [],
       nextMove: [],
       moveAfterNext: [],
@@ -16,13 +17,80 @@ class App extends React.Component {
   componentDidMount() {
 
   }
-  handleScoreInput() {
+
+  updateTurn() {
+    let el = document.getElementById(`turn-${this.state.currFrame}-${this.state.currTurn}`);
+    el.innerTexts = this.state.numPins;
+  }
+
+  updateFrame(frame) {
+    // passing in a frame, because for strikes and spares, I can't just capture currFrame
+    let el = document.getElementById(`frame-${frame}`);
+
+    el.innerText = 
+  }
+  
+  strike() {
+    let el = document.getElementById(`turn-${this.state.currFrame}-${this.state.currTurn}`);
+    el.innerTexts = this.state.numPins;
+  }
+  
+  spare() {
     
+  }
+  
+  turnOne() {
+    let pins = this.state.numPins;
+    if (pins === 10) {
+      this.strike();
+      this.setState((state) => {
+        return {currFrame: state.currFrame + 1};
+      })
+    } else {
+      this.updateTurn();
+      this.setState((state) => {
+        return {currTurn: state.currTurn + 1}
+      })
+    }
+  }
+  
+  turnTwo() {
+    let pins = this.state.numPins;
+    let firstTurn = document.getElementById(`turn-${this.state.currFrame}-1`).innerText;
+    if (pins + firstTurn === 10) {
+      this.spare();
+    }
+  }
+  
+  earlyFrames(e) {
+    if (this.state.currTurn === 1) {
+      this.turnOne();
+    } else if (currTurn === 2) {
+      this.turnTwo();
+    } else {
+      console.error('Turn toggling system error. Please see App.jsx')
+    }
+  }
+
+  finalFrame() {
+    console.log('finalFrame');
+  }
+
+  handleSubmit(e) {
+    if (this.state.currFrame <= 9) {
+      this.earlyFrames(e);
+    } else if (this.state.currFrame === 10) {
+      this.finalFrame(e);
+    } else (
+      this.setState({
+        message: 'Game over! Congratulations on your awesome score!!'
+      })
+    )
   }
   render() {
     return (
       <div>
-        
+        <form onSubmit={this.handleSubmit}></form>
         <Scorecard />
         {/* <MessageBoard /> */}
       </div>
--- challenge_3/client/components/Scorecard.jsx
+++ challenge_3/client/components/Scorecard.jsx
@@ -22,40 +22,40 @@ const Scorecard = (props) => {
         <tbody>
           <tr>
             <td>Player One</td>
-            <td id="score-1-1"></td>
-            <td id="score-1-2"></td>
-            <td id="score-2-1"></td>
-            <td id="score-2-2"></td>
-            <td id="score-3-1"></td>
-            <td id="score-3-2"></td>
-            <td id="score-4-1"></td>
-            <td id="score-4-2"></td>
-            <td id="score-5-1"></td>
-            <td id="score-5-2"></td>
-            <td id="score-6-1"></td>
-            <td id="score-6-2"></td>
-            <td id="score-7-1"></td>
-            <td id="score-7-2"></td>
-            <td id="score-8-1"></td>
-            <td id="score-8-2"></td>
-            <td id="score-9-1"></td>
-            <td id="score-9-2"></td>
-            <td id="score-10-1"></td>
-            <td id="score-10-2"></td>
-            <td id="score-10-3"></td>
+            <td id="turn-1-1"></td>
+            <td id="turn-1-2"></td>
+            <td id="turn-2-1"></td>
+            <td id="turn-2-2"></td>
+            <td id="turn-3-1"></td>
+            <td id="turn-3-2"></td>
+            <td id="turn-4-1"></td>
+            <td id="turn-4-2"></td>
+            <td id="turn-5-1"></td>
+            <td id="turn-5-2"></td>
+            <td id="turn-6-1"></td>
+            <td id="turn-6-2"></td>
+            <td id="turn-7-1"></td>
+            <td id="turn-7-2"></td>
+            <td id="turn-8-1"></td>
+            <td id="turn-8-2"></td>
+            <td id="turn-9-1"></td>
+            <td id="turn-9-2"></td>
+            <td id="turn-10-1"></td>
+            <td id="turn-10-2"></td>
+            <td id="turn-10-3"></td>
           </tr>
           <tr>
             <td>Score</td>
-            <td id="round-1"colspan="2"></td>
-            <td id="round-2"colspan="2"></td>
-            <td id="round-3"colspan="2"></td>
-            <td id="round-4"colspan="2"></td>
-            <td id="round-5"colspan="2"></td>
-            <td id="round-6"colspan="2"></td>
-            <td id="round-7"colspan="2"></td>
-            <td id="round-8"colspan="2"></td>
-            <td id="round-9"colspan="2"></td>
-            <td id="round-10"colspan="3"></td>
+            <td id="frame-1"colspan="2"></td>
+            <td id="frame-2"colspan="2"></td>
+            <td id="frame-3"colspan="2"></td>
+            <td id="frame-4"colspan="2"></td>
+            <td id="frame-5"colspan="2"></td>
+            <td id="frame-6"colspan="2"></td>
+            <td id="frame-7"colspan="2"></td>
+            <td id="frame-8"colspan="2"></td>
+            <td id="frame-9"colspan="2"></td>
+            <td id="frame-10"colspan="3"></td>
           </tr>
         </tbody>
       </table>
